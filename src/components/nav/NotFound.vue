9e441d3e4d08f6bf0b890ab130e741daf9428eb7
alekseypetrenko <apetrenko.job@gmail.com>
2021-01-17 23:12:26 +0200

    first commit

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="container">
+    <h1>Page is not found</h1>
+    <router-link to="/teams">Go to all teams</router-link>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  text-align: center;
+}
+</style>
---
+++ src/components/teams/TeamsFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Teams footer</h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
